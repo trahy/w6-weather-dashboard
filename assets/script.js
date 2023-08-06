9e3261d18c31da6188c69702382d619c499b62a9
unknown <trahy032@gmail.com>
2023-08-06 11:04:19 +0930

    adjusted API and updated styling

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,14 @@ var searchHistory = []
 
 // 
 // DELETE BEFORE COMMIT
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="
 var appID = "&units=metric&appid="
 var apiKey = "c3fc82326b5204d58c40763d162c915e"
 // DELETE BEFORE COMMIT
 // 
 
 // set current date
-$('#current-date').text(dayjs().format('(MM/DD/YYYY)'));
+$('#current-date').text(dayjs().format('ddd, MMMM D'));
 
 // calling weather api function
 var getWeather = function (city) {
@@ -20,21 +20,21 @@ var getWeather = function (city) {
         .then(function (response) {
             return response.json();
         })
-        // attaching data to file
+        // attaching data to doc
         .then(function (data) {
-            $("#city").text(data.name);
-            $("#temp_max").text(Math.round(data.main.temp_max) + "°C");
-            $("#temp_min").text(Math.round(data.main.temp_min) + "°C");
-            $("#wind").text(data.wind.speed + " km/h");
-            $("#humidity").text(data.main.humidity + "%");
+            $("#city").text(data.city.name + ", " + data.city.country);
+            $("#temp_max").text(Math.round(data.list[0].main.temp_max) + "°C");
+            $("#temp_min").text(Math.round(data.list[0].main.temp_min) + "°C");
+            $("#wind").text("Wind: " + data.list[0].wind.speed + " km/h");
+            $("#humidity").text("Humidity: " + data.list[0].main.humidity + "%");
             console.log(data);
         })
         .catch(function (error) {
             console.error(error);
         });
-
-
 }
 console.log(getWeather);
 
 getWeather();
+
+// var forecastAPI = forecastUrl + city + appID + apiKey
