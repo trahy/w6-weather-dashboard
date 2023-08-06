3adcaa5fedcc929a9a516ba10a30f8f917e0bc6f
unknown <trahy032@gmail.com>
2023-08-06 21:41:52 +0930

    updated styling

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # w6-weather-dashboard
+
+
+https://github.com/basmilius/weather-icons
--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,8 @@
-var searchHistory = []
 
-// 
-// DELETE BEFORE COMMIT
 var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="
 var appUnit = "&units=metric"
 var appID = "&appid=c3fc82326b5204d58c40763d162c915e"
-// DELETE BEFORE COMMIT
-// 
+
 
 // set current date
 $('#current-date').text(dayjs().format('ddd, MMMM D'));
@@ -29,7 +25,6 @@ var getWeather = function (city) {
 
 }
 
-
 // attaching data to doc
 function displayData(data) {
     $("#city").text(data.city.name + ", " + data.city.country);
@@ -37,20 +32,17 @@ function displayData(data) {
     $("#temp_min").text(Math.round(data.list[0].main.temp_min) + "°C");
     $("#wind").text("Wind: " + data.list[0].wind.speed + " km/h");
     $("#humidity").text("Humidity: " + data.list[0].main.humidity + "%");
-    $("#icon").attr("src",("assets/img/" + data.list[0].weather[0].icon + ".svg"));
+    $("#icon").attr("src", ("assets/img/" + data.list[0].weather[0].icon + ".svg"));
 
     console.log(data);
 }
 
+// function to handle search
 function handleSearch(event) {
     event.preventDefault();
 
     var input = $("#input").val();
 
-    // if (!input) {
-    //     console.error('You need to enter a city name.');
-    //     return;
-    // }
     if (!input) {
         $("#input").addClass("border border-danger border-3");
         $("#input").attr("placeholder", "✖ Input not valid.");
@@ -60,6 +52,7 @@ function handleSearch(event) {
     // }
     getWeather(input);
 }
-
-
+// eventAddListener for search
 $("#search").on("submit", handleSearch);
+
+var searchHistory = []
