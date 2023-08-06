bc903c124289f134a7e979e1b9e5d6c7162e6230
unknown <trahy032@gmail.com>
2023-08-06 16:21:56 +0930

    fixed event function

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,8 @@ var searchHistory = []
 // 
 // DELETE BEFORE COMMIT
 var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="
-var appID = "&units=metric&appid="
-var apiKey = "c3fc82326b5204d58c40763d162c915e"
+var appUnit = "&units=metric"
+var appID = "&appid=c3fc82326b5204d58c40763d162c915e"
 // DELETE BEFORE COMMIT
 // 
 
@@ -13,7 +13,7 @@ $('#current-date').text(dayjs().format('ddd, MMMM D'));
 
 // calling weather api function
 var getWeather = function (city) {
-    var weatherAPI = apiUrl + city + appID + apiKey;
+    var weatherAPI = apiUrl + city + appUnit + appID;
 
     // makes a request to the url
     fetch(weatherAPI)
@@ -22,12 +22,7 @@ var getWeather = function (city) {
         })
         // attaching data to doc
         .then(function (data) {
-            $("#city").text(data.city.name + ", " + data.city.country);
-            $("#temp_max").text(Math.round(data.list[0].main.temp_max) + "°C");
-            $("#temp_min").text(Math.round(data.list[0].main.temp_min) + "°C");
-            $("#wind").text("Wind: " + data.list[0].wind.speed + " km/h");
-            $("#humidity").text("Humidity: " + data.list[0].main.humidity + "%");
-            console.log(data);
+            displayData(data);
         })
         .catch(function (error) {
             console.error(error);
@@ -35,20 +30,28 @@ var getWeather = function (city) {
 }
 console.log(getWeather);
 
+
+function displayData(data) {
+    $("#city").text(data.city.name + ", " + data.city.country);
+    $("#temp_max").text(Math.round(data.list[0].main.temp_max) + "°C");
+    $("#temp_min").text(Math.round(data.list[0].main.temp_min) + "°C");
+    $("#wind").text("Wind: " + data.list[0].wind.speed + " km/h");
+    $("#humidity").text("Humidity: " + data.list[0].main.humidity + "%");
+
+    console.log(data);
+}
+
 function handleSearch(event) {
     event.preventDefault();
 
-    var searchInp = $('#search-inp').value;
+    var input = $("#input").val();
 
-    if (!searchInp) {
+    if (!input) {
         console.error('You need to enter a city name.');
         return;
     }
 
-    var getResult = apiUrl + searchInp + appID + apiKey;
-    location.assign(getResult);
+    getWeather(searchInp);
 }
 
 $("#search").on("submit", handleSearch);
-
-// var forecastAPI = forecastUrl + city + appID + apiKey
