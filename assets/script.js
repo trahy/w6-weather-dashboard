42580b2191c873aed7728cc29e3c8ef7007c87d3
unknown <trahy032@gmail.com>
2023-08-07 22:28:45 +0930

    created 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,6 @@ var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="
 var appUnit = "&units=metric"
 var appID = "&appid=c3fc82326b5204d58c40763d162c915e"
 
-
 // set current date
 var currentDate = $('#current-date').text(dayjs().format('ddd, MMMM D'));
 
@@ -18,18 +17,20 @@ var getWeather = function (city) {
         })
         .then(function (data) {
             displayWeather(data);
-        })
-        .then(function (data) {
             displayForecast(data);
         })
-
         .catch(function (error) {
             console.error(error);
+            handleError();
         });
 }
 
 // attaching data to doc
 function displayWeather(data) {
+    // Remove invalid classes
+    $("#input").removeClass("border border-danger border-3");
+    $("#input").attr("placeholder", "Enter city name");
+
     $("#city").text(data.city.name + ", " + data.city.country);
     $("#temp_max").text(Math.round(data.list[0].main.temp_max) + "°C");
     $("#temp_min").text(Math.round(data.list[0].main.temp_min) + "°C");
@@ -41,6 +42,10 @@ function displayWeather(data) {
     console.log(data);
 };
 
+function handleError() {
+    $("#input").addClass("border border-danger border-3");
+    $("#input").attr("placeholder", "✖ Input not valid.");
+}
 
 // function to handle search
 function handleSearch(event) {
@@ -49,37 +54,42 @@ function handleSearch(event) {
     var input = $("#input").val();
     // adds visual warning when search is invalid
     if (!input) {
-        $("#input").addClass("border border-danger border-3");
-        $("#input").attr("placeholder", "✖ Input not valid.");
+        return handleError();
     }
-    // else(){
-    //     return;
-    // }
+
     getWeather(input);
 };
 
 // eventAddListener for search
 $("#search").on("submit", handleSearch);
 
-// var searchHistory = []
-
 function displayForecast(data) {
+    if (data == null) {
+        return;
+    }
+
     // clears the forecast data
     $("#forecast").empty();
 
     // loops data over time period
     //  24 hours/ 3 hour api recall = 8th value = 1 day 
-    for (i = 1; i < data.list.length; i += 8) {
+    for (let i = 7; i < data.list.length; i += 8) {
+
+        let date = dayjs.unix(data.list[i].dt * 1000).format('ddd, MMMM D');
+        let img = `assets/img/${data.list[i].weather[0].icon}.svg`;
+        let temp = Math.round(data.list[i]["main"]["temp"]);
+        let wind = data.list[i]["wind"]["speed"];
+        let humidity = data.list[i]["main"]["humidity"];
 
         // insert data into forecast cards
         let forecastCards = `
         <div class="forecast-card col m-2 bg-white rounded py-4 shadow">
             <div class="forecast-body p-1">
-                <h5 class="forecast-date">`+ dayjs(data.list[i].dt * 1000).format('ddd, MMMM D')`</h5>
-                <img src="assets/img/"`+ data.list[i].weather[0].icon + `alt="weather icon">
-                <h3 class="forecast-temp">`+ data.list[i].main.temp`</h3>
-                <p class="forecast-wind">Wind: `+ data.list[i].wind.speed`</p>
-                <p class="forecast-humidity">`+ data.list[i].main.humidity`</p>
+                <h5 class="forecast-date">`+ date + `</h5>
+                <img src="`+ img +`" alt="weather icon">
+                <h3 class="forecast-temp">`+ temp + `°C</h3>
+                <p class="forecast-wind">Wind: `+ wind + `km/h</p>
+                <p class="forecast-humidity">Humidty: `+ humidity + `%</p>
             </div>
         </div>
         `;
@@ -88,5 +98,3 @@ function displayForecast(data) {
         $("#forecast").append(forecastCards);
     }
 };
-
-console.log(data.list)
